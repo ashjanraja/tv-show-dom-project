2a26ad979b7b0248493492699ef85135aae32725
ashjanraja <ashjannairat787@gmail.com>
2021-08-27 20:21:43 +0300

    up to date

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function zeroCheck(num) {
+  if (num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((item) => {
+    let episode = document.createElement("div");
+    episode.setAttribute("class", "episode");
+    rootElem.appendChild(episode);
+    let title = document.createElement("h3");
+    episode.appendChild(title);
+    title.setAttribute("class", "title");
+    title.innerHTML = `${item.name} - S${zeroCheck(item.season)}E${zeroCheck(
+      item.number
+    )} `;
+    let imgTag = document.createElement("img");
+    episode.appendChild(imgTag);
+    imgTag.src = `${item.image.medium}`;
+    let pSummary = document.createElement("p");
+    episode.appendChild(pSummary);
+    pSummary.innerHTML = `${item.summary}`;
+  });
 }
 
 window.onload = setup;
